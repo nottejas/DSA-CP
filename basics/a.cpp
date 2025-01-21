12af7ad1e814bee16723e5ba57fcf57352419bc7
nottejas <padmakartejas24@gmail.com>
2025-01-21 22:12:38 +0530

    still getting better

--- basics/a.cpp
+++ basics/a.cpp
@@ -2,7 +2,5 @@
 using namespace std;
 
 int main(){
-    int arr1[] = {1,2,3};
-    int arr2[] = {4,5,6};
-    // cout << arr1 + " " + arr2;
+    cout<<"Fucking sick! I just want to get better.";
 }
